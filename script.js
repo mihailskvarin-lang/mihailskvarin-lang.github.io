dd054305423a0f2f701faed7750c209e874c1c43
mihailskvarin-lang <mihailskvarin@gmail.com>
2026-05-03 22:41:37 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function handle() {
-    let inputs = document.querySelectorAll("input");
-    let input_values = [];
-    for (let i = 0; i < inputs.length; i++) {
-        input_values.push(inputs[i].value);
-    }
-}
+let links = document.querySelectorAll('.scroll');
+let targetID;
+links.forEach(function (element) {
+    element.addEventListener('click', function (event) {
+        event.preventDefault();
+        targetID = element.getAttribute('href');
+        document.querySelector(targetID).scrollIntoView( {
+            behavior: 'smooth',
+            block: 'start'
+         } )
+    } )
+} )
 
-let button = document.querySelector('.btn');
-button.addEventListener('click', handle);
+$(document).ready(function () {
+    $('.slider').bxSlider( {
+        pagerCustom: '.slider-nav',
+        infiniteLoop: false,
+        hideControlOnEnd: true,
+    } );
+} );
